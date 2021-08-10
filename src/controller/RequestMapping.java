868306e7a43dc5adb0875c53b23c69601fd028a9
janu01ary <janu01ary@gmail.com>
2021-08-10 13:36:47 +0900

    수정: login, register, exhibition, artwork보기 관련 내용 추가

--- src/controller/RequestMapping.java
+++ src/controller/RequestMapping.java
@@ -3,16 +3,36 @@ package controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import controller.WebVR.ListExhbController;
+import controller.WebVR.ViewArtworkController;
+import controller.WebVR.ViewExhbController;
+import controller.WebVR.user.LoginController;
+import controller.WebVR.user.LogoutController;
+import controller.WebVR.user.RegisterUserController;
+
 public class RequestMapping {
 	// 각 요청 uri에 대한 controller 객체를 저장할 HashMap 생성
 	private Map<String, Controller> mappings = new HashMap<String, Controller>();
 
 	public void initMapping() {
 		// 각 uri에 대응되는 controller 객체를 생성 및 저장
+
+		//home
+        mappings.put("/", new ListExhbController());
 		
-		// 작성 예시
-		// mappings.put("/artist/login/form", new ForwardController("/artist/login_register.jsp"));
-		// mappings.put("/artist/login", new LoginController());
+		//login, register
+        mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
+        mappings.put("/user/login", new LoginController());
+        mappings.put("/user/logout", new LogoutController());
+        mappings.put("/user/register/form", new ForwardController("/user/registerForm.jsp"));
+        mappings.put("/user/register", new RegisterUserController());
+        
+        //exhibition
+        mappings.put("/WebVR/exhb", new ViewExhbController());
+        
+        //artwork
+        mappings.put("/WebVR/exhb/artwork", new ViewArtworkController());
+        
 	}
 
 	public Controller findController(String uri) {
